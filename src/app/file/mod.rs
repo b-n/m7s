a49192163ab7eea8922695dfc2223417e33e36ec
Ben Naylor <nayben@gmail.com>
2026-01-05 07:49:21 +0100

    WIP

--- src/app/file/mod.rs
+++ src/app/file/mod.rs
@@ -1,25 +1,40 @@
 use log::debug;
-use ratatui::text::Line;
-use rowan::{ast::SyntaxNodePtr as RowanSyntaxNodePtr, NodeOrToken, WalkEvent};
+use ratatui::{
+    style::{Style, Stylize},
+    text::{Line, Span},
+};
+use rowan::{
+    ast::SyntaxNodePtr as RowanSyntaxNodePtr, NodeOrToken, TextSize,
+    TokenAtOffset as RowanTokenAtOffset, WalkEvent,
+};
 use std::path::PathBuf;
 use yaml_parser::{SyntaxKind, SyntaxNode, SyntaxToken, YamlLanguage};
 
-mod line;
 mod utils;
 
-type SyntaxNodePtr = RowanSyntaxNodePtr<YamlLanguage>;
+pub(crate) type SyntaxNodePtr = RowanSyntaxNodePtr<YamlLanguage>;
+pub(crate) type TokenAtOffset = RowanTokenAtOffset<SyntaxToken>;
 
-pub use line::FileLine;
+use utils::{
+    ancestor_not_kind, line_at_cursor, node_dimensions, selectable_kind, token_at_cursor,
+    token_in_direction,
+};
 
 // TODO: Save file
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct File {
     path: PathBuf,
     pub max_width: usize,
     pub line_count: usize,
-    pub max_selectable: usize,
-    lines: Vec<FileLine>,
     ast: SyntaxNode,
+}
+
+#[derive(Debug)]
+pub enum Direction {
+    Up(usize),
+    Down(usize),
+    Left(usize),
+    Right(usize),
 }
 
 impl File {
@@ -29,39 +44,69 @@ impl File {
         let raw = std::fs::read_to_string(&path).unwrap();
 
         let ast = yaml_parser::parse(&raw).unwrap();
-        let lines = tree_to_lines(&ast);
 
-        let max_selectable = lines.iter().map(FileLine::selectable).max().unwrap_or(0);
-        let max_width = lines.iter().map(FileLine::length).max().unwrap_or(0);
-        let line_count = lines.len();
+        let (line_count, max_width) = node_dimensions(&ast);
 
         Self {
             path,
             max_width,
             line_count,
-            max_selectable,
-            lines,
             ast,
         }
     }
 
-    pub fn render(&self, cursor: (usize, usize)) -> (Vec<Line<'_>>, usize) {
-        //let lines = self
-        //    .lines
-        //    .iter()
-        //    .enumerate()
-        //    .map(|(i, line)| line.render(i, cursor))
-        //    .collect();
-
-        //(lines, self.max_width)
-        tree_to_lines_two(&self.ast)
+    pub fn render(&self, cursor: usize) -> (Vec<Line<'_>>, usize) {
+        tree_to_lines(&self.ast, cursor.try_into().unwrap())
     }
 
-    pub fn info(&self, cursor: (usize, usize)) {
-        let token: Option<SyntaxNodePtr> = self
-            .lines
-            .get(cursor.0)
-            .and_then(|line| line.selectable_token_at(cursor.1));
+    pub fn line_at_cursor(&self, cursor: u32) -> usize {
+        line_at_cursor(&self.ast, cursor)
+    }
+
+    pub fn cursor_at_line(&self, line: usize) -> u32 {
+        let mut line_count = 0;
+        let mut selected = token_at_cursor(&self.ast, 0);
+
+        // scroll to the line
+        while let Some(ref next) = selected {
+            let text = next.text();
+            line_count += text.chars().filter(|c| *c == '\n').count();
+            if line_count >= line {
+                break;
+            }
+
+            selected = next.next_token();
+        }
+
+        // Find the first selectable token
+        while let Some(ref next) = selected {
+            if selectable_kind(next.kind()) {
+                break;
+            }
+            selected = next.next_token();
+        }
+
+        selected
+            .expect("Should always have a token")
+            .text_range()
+            .start()
+            .into()
+    }
+
+    // Takes the current cursor position, and returns the cursor position of the relevant next
+    // token.
+    pub fn navigate_dir(&self, current_cursor: u32, direction: &Direction) -> u32 {
+        let current_token = token_at_cursor(&self.ast, current_cursor)
+            .expect("Cursor should always be at a valid token");
+
+        token_in_direction(&current_token, direction)
+            .text_range()
+            .start()
+            .into()
+    }
+
+    pub fn info(&self, cursor: u32) {
+        let token = token_at_cursor(&self.ast, cursor);
 
         debug!("Cursor: {cursor:?}");
         debug!("Token: {token:?}");
@@ -74,124 +119,49 @@ impl File {
     }
 }
 
-// Processing logic:
-// - Each node can have child tokens and child nodes
-// - Tokens are collected under their parent node
-// - Nodes and Tokens are collected by newlines
-// - Newlines only exist in whitespace tokens
-//
-// Important:
-// - A line can have many collections of tokens.
-// - Each collection of tokens should be associated with it's parent node
-//
-// Therefore:
-// - We traverse the tree in order
-// - For each token, it is added to a buffer
-// - Since a node can have both nodes and tokens as children, the buffer needs to be flushed on
-//   both entry and exit of nodes
-// - If a node is entered or exited:
-//   - Flush the token buffer to the `pending_line` with the previous last known node
-//   - Update the last known node. Entry: current, Exit: parent of current
-fn tree_to_lines(tree: &SyntaxNode) -> Vec<FileLine> {
-    let mut lines = Vec::new();
-
-    let mut pending_line = FileLine::new(None);
-    let mut last_node = SyntaxNodePtr::new(tree);
-    let mut token_buffer: Vec<SyntaxToken> = Vec::new();
-
-    for event in tree.preorder_with_tokens() {
-        match event {
-            WalkEvent::Enter(element) => match element {
-                NodeOrToken::Node(node) => {
-                    debug!("++node: {node:?}");
-                    // A node may have tokens and children, so we need to flush the buffered tokens
-                    // before moving down the tree
-                    if !token_buffer.is_empty() {
-                        pending_line.add_tokens(last_node, token_buffer.clone(), tree);
-                        token_buffer.clear();
-                    }
-
-                    // Moving the parent down the tree
-                    last_node = SyntaxNodePtr::new(&node);
-                }
-                NodeOrToken::Token(token) => {
-                    debug!("++token: {token:?} {:?}", token.text());
-                    match token.kind() {
-                        SyntaxKind::WHITESPACE => {
-                            let mut split_newlines = token.text().split('\n').peekable();
-
-                            // Get the first element, it'll always have some value
-                            let ws = split_newlines
-                                .next()
-                                .expect("Whitespace elements should always have some value");
-
-                            if split_newlines.peek().is_none() {
-                                // No newlines, just whitespace
-                                token_buffer.push(token.clone());
-                                continue;
-                            }
-
-                            // There is a newline, so we store the first part of the split in the
-                            // current line (could be trailing whitespace)
-                            pending_line.trailing_whitespace(Some(ws.to_string()));
-
-                            // We then process all remaining parts. Each part drops any whitespace
-                            // into a new line
-                            for line in split_newlines {
-                                // Store the active token
-                                pending_line.add_tokens(last_node, token_buffer.clone(), tree);
-                                lines.push(pending_line);
-
-                                token_buffer.clear();
-                                // Note, there is no need to store the extra whitespace as it's
-                                // own token, since we split it and add to preceding_whitespace
-                                pending_line = FileLine::new(Some(line.to_string()));
-                            }
-                        }
-                        _ => {
-                            token_buffer.push(token.clone());
-                        }
-                    }
-                }
-            },
-            WalkEvent::Leave(element) => match element {
-                NodeOrToken::Node(node) => {
-                    debug!("--node {node:?}");
-
-                    if !token_buffer.is_empty() {
-                        pending_line.add_tokens(last_node, token_buffer.clone(), tree);
-                        token_buffer.clear();
-                    }
-
-                    // Move the last_node up the tree when exiting a node
-                    if let Some(parent) = node.parent() {
-                        last_node = SyntaxNodePtr::new(&parent);
-                    }
-                }
-                NodeOrToken::Token(token) => {
-                    debug!("--token {:?}", token.kind());
-                }
-            },
-        }
+fn style(kind: SyntaxKind) -> Style {
+    match kind {
+        SyntaxKind::BLOCK_MAP_KEY => Style::default().bold().fg(ratatui::style::Color::Yellow),
+        _ => Style::default(),
     }
-
-    lines
 }
 
-fn tree_to_lines_two(tree: &SyntaxNode) -> (Vec<Line<'_>>, usize) {
+fn styled_span(s: String, kind: SyntaxKind, active: bool) -> Span<'static> {
+    let mut span = Span::from(s);
+    span = span.style(style(kind));
+    if active {
+        span = span.reversed();
+    }
+    span
+}
+
+fn tree_to_lines(tree: &SyntaxNode, cursor: u32) -> (Vec<Line<'_>>, usize) {
     let mut lines = Vec::new();
     let mut max_width = 0;
 
-    let mut pending_line = String::new();
+    let mut pending_line = vec![];
+    let mut last_node = None;
 
     for event in tree.preorder_with_tokens() {
         match event {
             WalkEvent::Enter(element) => match element {
                 NodeOrToken::Node(node) => {
                     debug!("++node: {node:?}");
+                    last_node = Some(SyntaxNodePtr::new(&node));
                 }
                 NodeOrToken::Token(token) => {
                     debug!("++token: {token:?} {:?}", token.text());
+
+                    let active_token = token.text_range().contains(TextSize::new(cursor));
+
+                    let parent_kind = ancestor_not_kind(
+                        last_node
+                            .expect("Tokens always have parent Nodes")
+                            .to_node(tree),
+                        SyntaxKind::FLOW,
+                    )
+                    .unwrap()
+                    .kind();
 
                     let mut split_newlines = token.text().split('\n').peekable();
 
@@ -199,21 +169,27 @@ fn tree_to_lines_two(tree: &SyntaxNode) -> (Vec<Line<'_>>, usize) {
                     let tok = split_newlines
                         .next()
                         .expect("Whitespace elements should always have some value");
-                    pending_line += tok;
+                    pending_line.push(styled_span(tok.to_string(), parent_kind, active_token));
 
                     for line in split_newlines {
                         let line_len = pending_line.len();
                         if line_len > max_width {
                             max_width = line_len;
                         }
-                        lines.push(Line::from(pending_line));
-                        pending_line = line.to_string();
+                        lines.push(Line::from(pending_line.clone()));
+                        pending_line.clear();
+                        pending_line.push(styled_span(line.to_string(), parent_kind, active_token));
                     }
                 }
             },
             WalkEvent::Leave(element) => match element {
                 NodeOrToken::Node(node) => {
                     debug!("--node {node:?}");
+                    last_node = if let Some(parent) = node.parent() {
+                        Some(SyntaxNodePtr::new(&parent))
+                    } else {
+                        None
+                    };
                 }
                 NodeOrToken::Token(token) => {
                     debug!("--token {:?}", token.kind());
@@ -222,7 +198,8 @@ fn tree_to_lines_two(tree: &SyntaxNode) -> (Vec<Line<'_>>, usize) {
         }
     }
 
-    lines.push(Line::from(pending_line));
+    lines.push(Line::from(pending_line.clone()));
+    pending_line.clear();
 
     (lines, max_width)
 }
--- src/app/file/utils.rs
+++ src/app/file/utils.rs
@@ -1,4 +1,7 @@
-use yaml_parser::{SyntaxKind, SyntaxNode};
+use rowan::{NodeOrToken, TextSize, WalkEvent};
+use yaml_parser::{SyntaxKind, SyntaxNode, SyntaxToken};
+
+use super::{Direction, TokenAtOffset};
 
 pub(crate) fn ancestor_not_kind(node: SyntaxNode, kind: SyntaxKind) -> Option<SyntaxNode> {
     if node.kind() == kind {
@@ -11,12 +14,191 @@ pub(crate) fn ancestor_not_kind(node: SyntaxNode, kind: SyntaxKind) -> Option<Sy
     Some(node)
 }
 
-pub(crate) fn is_selectable_kind(kind: SyntaxKind) -> bool {
+pub(crate) fn selectable_kind(kind: SyntaxKind) -> bool {
     matches!(
         kind,
-        SyntaxKind::BLOCK_MAP_KEY
-            | SyntaxKind::BLOCK_MAP_VALUE
-            | SyntaxKind::DOCUMENT
-            | SyntaxKind::COMMENT
+        SyntaxKind::COMMENT | SyntaxKind::BLOCK_SCALAR_TEXT | SyntaxKind::PLAIN_SCALAR
     )
+}
+
+pub(crate) fn node_dimensions(tree: &SyntaxNode) -> (usize, usize) {
+    let mut max_width = 0;
+    let mut line_count = 0;
+
+    let mut pending_line = String::new();
+
+    for event in tree.preorder_with_tokens() {
+        if let WalkEvent::Enter(element) = &event
+            && let NodeOrToken::Token(token) = element
+        {
+            let token_text = token.text();
+            let mut split_newlines = token_text.split('\n').peekable();
+            // Get the first element, it'll always have some value
+            let tok = split_newlines
+                .next()
+                .expect("Tokens always have some content");
+
+            pending_line.push_str(tok);
+
+            for line in split_newlines {
+                let line_len = pending_line.len();
+                if line_len > max_width {
+                    max_width = line_len;
+                }
+                line_count += 1;
+                pending_line.clear();
+                pending_line.push_str(line);
+            }
+        }
+    }
+
+    (line_count, max_width)
+}
+
+pub(crate) fn line_at_cursor(tree: &SyntaxNode, cursor: u32) -> usize {
+    let mut line_count = 0;
+
+    for event in tree.preorder_with_tokens() {
+        if let WalkEvent::Enter(element) = &event
+            && let NodeOrToken::Token(token) = element
+        {
+            if token.text_range().contains(TextSize::new(cursor)) {
+                break;
+            }
+            let text = token.text();
+            let without_newlines = text.replace('\n', "");
+            let newlines = text.len() - without_newlines.len();
+
+            line_count += newlines;
+        }
+    }
+
+    line_count
+}
+
+pub(crate) fn token_at_cursor(
+    tree: &SyntaxNode,
+    cursor: u32,
+) -> Option<rowan::SyntaxToken<yaml_parser::YamlLanguage>> {
+    match tree.token_at_offset(TextSize::new(cursor)) {
+        TokenAtOffset::Single(token) => Some(token),
+        TokenAtOffset::Between(_, token) => {
+            // Always favour the righthand token. Range ends are inclusive
+            Some(token)
+        }
+        TokenAtOffset::None => None,
+    }
+}
+
+fn token_is_newlined_whitespace(token: &SyntaxToken) -> bool {
+    token.kind() == SyntaxKind::WHITESPACE && token.text().contains('\n')
+}
+
+fn first_selectable_in_line(token: &SyntaxToken) -> SyntaxToken {
+    let mut selected = token.clone();
+
+    let mut next_token = token.prev_token();
+    while let Some(ref next) = next_token {
+        if token_is_newlined_whitespace(next) {
+            break;
+        }
+        if selectable_kind(next.kind()) {
+            selected = next.clone();
+        }
+
+        next_token = next.prev_token();
+    }
+
+    selected
+}
+
+pub(crate) fn token_in_direction(token: &SyntaxToken, dir: &Direction) -> SyntaxToken {
+    let mut selected = token.clone();
+    match dir {
+        Direction::Up(n) | Direction::Down(n) | Direction::Left(n) | Direction::Right(n)
+            if *n == 0 => {}
+        Direction::Up(n) => {
+            let mut newlines = 0;
+            let mut next_token = token.prev_token();
+            while let Some(ref next) = next_token {
+                if selectable_kind(next.kind()) {
+                    selected = next.clone();
+                }
+
+                if newlines >= *n {
+                    break;
+                }
+
+                if token_is_newlined_whitespace(next) {
+                    newlines += 1;
+                }
+
+                next_token = next.prev_token();
+            }
+
+            // The above will find the last token in the line. That feels weird. Use the first
+            // token instead
+            selected = first_selectable_in_line(&selected);
+        }
+        Direction::Down(n) => {
+            let mut newlines = 0;
+            let mut next_token = token.next_token();
+            while let Some(ref next) = next_token {
+                if selectable_kind(next.kind()) {
+                    selected = next.clone();
+                }
+                if newlines >= *n && selectable_kind(next.kind()) {
+                    break;
+                }
+                if token_is_newlined_whitespace(next) {
+                    newlines += 1;
+                }
+
+                next_token = next.next_token();
+            }
+            // If we didn't go anywhere, return the input value
+            if newlines == 0 {
+                return token.clone();
+            }
+        }
+        Direction::Right(n) => {
+            let mut tokens = 0;
+            let mut next_token = token.next_token();
+            while let Some(ref next) = next_token {
+                if selectable_kind(next.kind()) {
+                    selected = next.clone();
+                    tokens += 1;
+                    if tokens >= *n {
+                        break;
+                    }
+                }
+
+                if token_is_newlined_whitespace(next) {
+                    break;
+                }
+
+                next_token = next.next_token();
+            }
+        }
+        Direction::Left(n) => {
+            let mut tokens = 0;
+            let mut next_token = token.prev_token();
+            while let Some(ref next) = next_token {
+                if selectable_kind(next.kind()) {
+                    selected = next.clone();
+                    tokens += 1;
+                    if tokens >= *n {
+                        break;
+                    }
+                }
+
+                if token_is_newlined_whitespace(next) {
+                    break;
+                }
+
+                next_token = next.prev_token();
+            }
+        }
+    }
+    selected
 }
